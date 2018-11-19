963c187f55c8af070f670c1f3e58577516138752
Lisa Griere <lisa.gr@hotmail.fr>
2018-11-19 01:16:58 +0100

    Lab2

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
 // Import a module
-const http = require('http')
-const handles = require('./handles')
-const server = http.createServer(handles.serverHandle);
-server.listen(8080)
+express = require('express')
+app = express()
+path = require('path')
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('port', 8080)
+app.set('views', __dirname + "/views")
+app.set('view engine', 'ejs');
+
+app.listen(
+  app.get('port'),
+  () => console.log(`server listening on ${app.get('port')}`)
+)
+
+app.get( '/',
+  (req, res) => res.render('explanations.ejs')
+)
+
+app.get('/hello/:name',
+  (req, res) => {
+    if(req.params.name === "Lisa"){
+		res.render('introduction.ejs', {name: req.params.name})	
+	}
+    else {
+		res.render('hello.ejs', {name: req.params.name})
+    }
+  }
+)
+
+/*
+app.post('/', (req, res) => {
+  // POST
+})
+
+app
+  .put('/', function (req, res) {
+    // PUT
+  })
+  
+app.delete('/', (req, res) => {
+    // DELETE
+  })
+  */
+
+
